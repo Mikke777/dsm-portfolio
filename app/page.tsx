5cdd5067a0f83a8c59b4a6a1a7e56fe605cc9ea3
michele di stadio <distadiomichele93@gmail.com>
2024-11-22 15:18:13 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import About from "@/components/About";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { ModeToggle } from "@/components/ModeToggle";
+import { FaHome } from "react-icons/fa";
+import { FcAbout } from "react-icons/fc";
+import { GrProjects } from "react-icons/gr";
 
 export default function Home() {
   return (
     <main className="relative bg-background flex
     justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5 mt-5">
-      <div className="max-w-7xl w-full">
+      <div className="max-w-7xl w-full mt-1">
+        <FloatingNav navItems={[
+          { name: "Home", link: "/", icon: <FaHome /> },
+          { name: "About", link: "#about", icon: <FcAbout /> },
+          { name: "Projects", link: "#AppleCardsCarouselDemo", icon: <GrProjects /> },
+        ]} />
         <ModeToggle />
         <Hero />
         <About />
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
     <BackgroundBeamsWithCollision>
       <div className='flex justify-center relative my-20 z-10'>
           <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-            <h2 className='uppercase tracking-widest text-xs text-center dark:text-blue-100 max-w-80'>
+            <h2 className='uppercase tracking-widest text-xs text-center text-color max-w-80'>
               I&apos;m a software engineer and a designer
             </h2>
             <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words='Build your next project with Next.js' />
