8e3057289308abf50e782ac49e6353f644e19971
michele di stadio <distadiomichele93@gmail.com>
2024-11-24 13:06:14 +0100

    add timing

--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -7,7 +7,7 @@ export const TextGenerateEffect = ({
   words,
   className,
   filter = true,
-  duration = 0.5,
+  duration = 2,
 }: {
   words: string;
   className?: string;
@@ -16,18 +16,23 @@ export const TextGenerateEffect = ({
 }) => {
   const [scope, animate] = useAnimate();
   let wordsArray = words.split(" ");
+
   useEffect(() => {
-    animate(
-      "span",
-      {
-        opacity: 1,
-        filter: filter ? "blur(0px)" : "none",
-      },
-      {
-        duration: duration ? duration : 1,
-        delay: stagger(0.2),
-      }
-    );
+    const timeoutId = setTimeout(() => {
+      animate(
+        "span",
+        {
+          opacity: 1,
+          filter: filter ? "blur(0px)" : "none",
+        },
+        {
+          duration: duration ? duration : 1,
+          delay: stagger(1.5),
+        }
+      );
+    }, 2000); // 0.5-second delay
+
+    return () => clearTimeout(timeoutId); // Cleanup timeout on unmount
   }, [scope.current]);
 
   const renderWords = () => {
@@ -37,7 +42,7 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="text-black opacity-0"
+              className="opacity-0"
               style={{
                 filter: filter ? "blur(10px)" : "none",
               }}
@@ -52,8 +57,8 @@ export const TextGenerateEffect = ({
 
   return (
     <div className={cn("font-bold", className)}>
-      <div className="mt-4">
-        <div className="  text-color text-2xl leading-snug tracking-wide">
+      <div className="mt-0">
+        <div className="text-color text-6xl leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
