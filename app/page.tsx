3d4b487e6c5d4b2b8d3e3c932a883502ae3219a8
michele di stadio <distadiomichele93@gmail.com>
2024-11-27 15:02:31 +0100

    fix nav

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Hero from "@/components/Hero";
 import Footer from "@/components/Footer";
 import About from "@/components/About";
 import { Projects } from "@/components/Projects";
-import { FloatingNav } from "@/components/ui/FloatingNavbar";
+// import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { ModeToggle } from "@/components/ModeToggle";
-import { FaHome } from "react-icons/fa";
-import { FcAbout } from "react-icons/fc";
-import { GrProjects } from "react-icons/gr";
+// import { FaHome } from "react-icons/fa";
+// import { FcAbout } from "react-icons/fc";
+// import { GrProjects } from "react-icons/gr";
 
 export default function Home() {
   return (
@@ -14,11 +14,11 @@ export default function Home() {
     justify-center items-center flex-col overflow-hidden
     mx-auto sm:px-10 px-5 mt-5">
       <div className="max-w-7xl w-full mt-1">
-        <FloatingNav navItems={[
+        {/* <FloatingNav navItems={[
           { name: "Home", link: "/", icon: <FaHome /> },
           { name: "About", link: "#about", icon: <FcAbout /> },
           { name: "Projects", link: "#AppleCardsCarouselDemo", icon: <GrProjects /> },
-        ]} />
+        ]} /> */}
         <ModeToggle />
         <Hero />
         <About />
